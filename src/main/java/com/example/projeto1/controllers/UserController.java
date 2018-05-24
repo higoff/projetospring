78a7143508f255fb3d8c032753349283d53455f1
Higo Fernandes Ferreira <higoff@gmail.com>
2018-05-24 15:37:20 -0300

    - Alterações até a aula 23

--- src/main/java/com/example/projeto1/controllers/UserController.java
+++ src/main/java/com/example/projeto1/controllers/UserController.java
@@ -1,23 +1,21 @@
 package com.example.projeto1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.projeto1.repository.UserRepository;
+import com.example.projeto1.service.UserService;
 
 @Controller
 public class UserController {
 	
-	private UserRepository userRepository;
-	
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-	
+	@Autowired
+	private UserService userService;
+		
 	@RequestMapping("/user")
 	public String getUsers(Model model) {
-		model.addAttribute("usersList", this.userRepository.findAll());
+		model.addAttribute("usersList", this.userService.findAll());
 		
 		return "user";
 	}
